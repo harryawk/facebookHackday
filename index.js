383bdd9e4dd220cfa2025f553873540cb9056b1f
harryawk <harry.kambey@gmail.com>
2017-11-17 11:55:12 +0700

    test text formatting

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {    
 
     // Create the payload for a basic text message
-    var basic_requirements = 'Test Requirements: Basic requirements\nAlmond grows best in Mediterranean climates with warm, dry summers and mild, wet winters. The optimal temperature for their growth is between 15 and 30\xc2\xb0C (60\xe2\x80\x9385\xc2\xb0F) and the tree buds have a chilling requirement of between 300 and 600 hours below 7.2\xc2\xb0C (45\xc2\xb0F) to break dormancy. '.decode('utf8')
+    var basic_requirements = decodeURIComponent('Test Requirements: Basic requirements\nAlmond grows best in Mediterranean climates with warm, dry summers and mild, wet winters. The optimal temperature for their growth is between 15 and 30\xc2\xb0C (60\xe2\x80\x9385\xc2\xb0F) and the tree buds have a chilling requirement of between 300 and 600 hours below 7.2\xc2\xb0C (45\xc2\xb0F) to break dormancy. ')
     response = {
       "text": `You sent the message: "${received_message.text}". Now send me an image!\n\nTest Derajat: 30\xb0C . Test Ranging: 60\u201385\xb0F\n\n"${basic_requirements}"`
     }
