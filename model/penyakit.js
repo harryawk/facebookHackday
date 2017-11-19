663a15bbb960d0c9318b34739c3bac2a8d38a889
Harry Alvin Waidan Kefas <harry.kambey@gmail.com>
2017-11-19 07:16:41 +0700

    update

--- model/penyakit.js
+++ model/penyakit.js
@@ -1,7 +1,11 @@
 var helper = require('./database.js');
+require('./tanaman.js')
 
 Model = helper.bookshelf.Model.extend({
-  tableName: 'penyakit'
+  tableName: 'penyakit',
+  tanaman: function() {
+    return this.belongsTo('Tanaman', 'tanaman_id')
+  }
 });
 model = helper.bookshelf.model('Penyakit', Model);
 
